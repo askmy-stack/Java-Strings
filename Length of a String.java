e2a32007364cd0f6696fbeeaca90c1cec5ccb6ec
Abhinaysai Kamineni <66816045+askmy-stack@users.noreply.github.com>
2022-06-17 16:58:12 +0530

    Create Length of a String.java

---
+++ Length of a String.java
@@ -0,0 +1,12 @@
+package com.company;
+import java.util.Scanner;
+
+public class Practice {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the String:");
+        String a  = sc.nextLine();
+        int n = a.length();
+        System.out.print("Length of the String:"+n);
+    }
+ }
